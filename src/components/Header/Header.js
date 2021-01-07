edaa8dac034464612e47c303c5340c32b1feea81
Mike Scriver <mikescriver@gmail.com>
2021-01-07 17:25:56 -0600

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,32 +31,32 @@ function Header() {
             >
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="/">About</a>
-                        {/* <Link
+                        {/* <a className="nav-link" href="/">About</a> */}
+                        <Link
                             to="/"
                             className={location.pathname === "/" ? "nav-link active" : "nav-link"}
                         >
                             About
-                        </Link> */}
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a class="nav-link" href="/portfolio">Portfolio</a>
-                        {/* <Link
+                        {/* <a class="nav-link" href="/portfolio">Portfolio</a> */}
+                        <Link
                             to="/portfolio"
                             className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
                         >
                             Portfolio
-                        </Link> */}
+                        </Link>
 
                     </li>
                     <li className="nav-item">
-                        <a class="nav-link" href="/contact">Contact</a>
-                        {/* <Link
+                        {/* <a class="nav-link" href="/contact">Contact</a> */}
+                        <Link
                             to="/contact"
                             className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
                         >
                             Contact
-                        </Link> */}
+                        </Link>
 
                     </li>
                 </ul>
